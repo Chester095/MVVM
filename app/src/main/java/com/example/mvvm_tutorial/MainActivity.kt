157a123bda231e2554b5d337ba19ee4cd8582d59
Chester095 <50018523+Chester095@users.noreply.github.com>
2020-10-30 15:22:24 +0300

    mediatorLiveData

--- app/src/main/java/com/example/mvvm_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_tutorial/MainActivity.kt
@@ -2,22 +2,44 @@ package com.example.mvvm_tutorial
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.lifecycle.Observer
+import androidx.lifecycle.Transformations
 
 
 class MainActivity : AppCompatActivity() {
 
-    val ldString = MutableLiveData<String>()
-    val ldInt = MutableLiveData<Int>()
+    val liveDataString = MutableLiveData<String>()
+    val liveDataString2 = MutableLiveData<String>()
+    val mediatorLiveData = MediatorLiveData<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        liveDataString.value = "string"
+        liveDataString2.value = "string2"
 
-        ldString.value = "ldString"
-        ldInt.value = 1
+        mediatorLiveData.addSource(liveDataString)  {
+            test_text.text = it
+        }
+
+        mediatorLiveData.addSource(liveDataString2)  {
+            test_text1.text = it
+        }
+
+        mediatorLiveData.observe(this, Observer {
+
+        })
+
+        button_save.setOnClickListener{
+            liveDataString.value = edit_text.text.toString()
+        }
+
+        button_save2.setOnClickListener{
+            liveDataString2.value = edit_text.text.toString()
+        }
 
     }
 
